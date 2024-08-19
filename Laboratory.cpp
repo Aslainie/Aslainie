c94cccfa039d4fa5f5f7422cf7d75936ac437931
Aslainie <maruhomaslainie1@gmail.com>
2024-08-19 11:57:49 +0800

    Add files via upload

---
+++ Laboratory.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int number;
+	
+	cout << "\t MAIN MENU \n";
+	cout << "---------------------------- \n";
+	cout << "1. To Display List of Files \n";
+	cout << "2. Create New Directory \n";
+	cout << "3. To Change the Working Directory \n";
+	cout << "4. Exit \n";
+	cout << "Enter the Number: ";
+	cin  >> number;
+	
+	switch (number) {
+		case 1:
+			int myNumber;
+			
+			cout << "\n\t LIST FILE DETAIL \n";
+			cout << "---------------------------- \n";
+			cout << "1. List All Files \n";
+			cout << "2. List of Extension Files \n";
+			cout << "3. List of Name Wise \n";
+			cout << "Enter the Number: ";
+			cin  >>  myNumber;
+	}
+	
+}
