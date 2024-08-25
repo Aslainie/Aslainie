f7825818fccae8d87281c580d5cd5cece0d341fd
Aslainie <maruhomaslainie1@gmail.com>
2024-08-25 14:00:00 +0800

    Updated
    nag add ako ng change directory function

--- Laboratory.cpp
+++ Laboratory.cpp
@@ -23,8 +23,10 @@ int main() {
 				createNewDirectory();
 				break;
 			case 3:
+				changeDirectory();
 				break;
 			case 4:
+				
 				break;
 			default:
 				cout << "Invalid number. Please try again! \n"; 					
@@ -47,7 +49,7 @@ void listFiles() {
 void createNewDirectory() {
 
     string directoryName;
-    char currentPath[FILENAME_MAX];
+    char currentDirectory[FILENAME_MAX];
     
     cout << "Enter the directory name: ";
     cin >> directoryName;
@@ -59,11 +61,80 @@ void createNewDirectory() {
 	        cout << "Directory exists already.\n\n";
 	    }
 
-	    if (_getcwd(currentPath, sizeof(currentPath)) != NULL) {
-	        cout << "Current Directory: " << currentPath << "\\" << directoryName << "\n\n";
+	    if (_getcwd(currentDirectory, sizeof(currentDirectory)) != NULL) {
+	        cout << "Current Directory: " << currentDirectory << "\\" << directoryName << "\n\n";
 	    }
 	    system("pause");
 	    system("cls");
+}
+
+void changeDirectory() {
+    int option;
+    string directoryName;
+
+    char currentDirectory[FILENAME_MAX];
+    if (_getcwd(currentDirectory, sizeof(currentDirectory)) != NULL) {
+        cout << "\nCurrent Directory: " << currentDirectory << "\n";
+    } 
+
+    cout << "\tChange Directory \n";
+    cout << "------------------------------ \n";
+    cout << "1. Move to Parent Directory \n";
+    cout << "2. Move to Root Directory \n";
+    cout << "3. Move to a Specific Directory \n";
+    cout << "Enter your choice: ";
+    cin >> option;
+
+    switch (option) {
+        case 1:
+            if (_chdir("..") == 0) {
+                if (_getcwd(currentDirectory, sizeof(currentDirectory)) != NULL) {
+                    cout << "\nMoved to Parent Directory.\n";
+                    cout << "Current Directory: " << currentDirectory << "\n";
+                } 
+				else {
+                    cout << "\nFailed to retrieve the current directory after moving.\n";
+                }
+            } 
+			else {
+                cout << "\nFailed to move to Parent Directory.\n";
+            }
+            break;
+        case 2:
+            if (_chdir("/") == 0) {
+                if (_getcwd(currentDirectory, sizeof(currentDirectory)) != NULL) {
+                    cout << "\nMoved to Root Directory.\n";
+                    cout << "Current Directory: " << currentDirectory << "\n";
+                } 
+				else {
+                    cout << "\nFailed to retrieve the current directory after moving.\n";
+                }
+            } 
+			else {
+                cout << "\nFailed to move to Root Directory.\n";
+            }
+            break;
+        case 3:
+            cout << "\nEnter the path of the directory: ";
+            cin >> directoryName;
+
+            if (_chdir(directoryName.c_str()) == 0) {
+                if (_getcwd(currentDirectory, sizeof(currentDirectory)) != NULL) {
+                    cout << "\nChanged to Directory: " << currentDirectory << "\n";
+                } 
+				else {
+                    cout << "\nFailed to retrieve the current directory after moving.\n";
+                }
+            } 
+			else {
+                cout << "\nDirectory does not exist or failed to change directory.\n";
+            }
+            break;
+        default:
+            cout << "\nInvalid choice. Please try again.\n";
+    }
+    system("pause");
+    system("cls");
 }
 
 void displayMainMenu() {
